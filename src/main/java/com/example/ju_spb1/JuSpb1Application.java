e114ca402d1a445822b63b5163f85fe7d4a660a7
zhucheng <zhucheng@agree.com.cn>
2022-06-15 21:50:15 +0800

    Add first RequestMapping

--- src/main/java/com/example/ju_spb1/JuSpb1Application.java
+++ src/main/java/com/example/ju_spb1/JuSpb1Application.java
@@ -2,9 +2,17 @@ package com.example.ju_spb1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class JuSpb1Application {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello Juching!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JuSpb1Application.class, args);
