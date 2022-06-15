c1c9502d75cd17e4370135778925f81c90e75901
zhucheng <zhucheng@agree.com.cn>
2022-06-15 21:44:53 +0800

    Initial Spring Boot Project

---
+++ src/main/java/com/example/ju_spb1/JuSpb1Application.java
@@ -0,0 +1,13 @@
+package com.example.ju_spb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuSpb1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuSpb1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ju_spb1/JuSpb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ju_spb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuSpb1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
